fa26dd40504056c7159fc25d77ab493649702fb6
Jeremy-lal <jlalait33700@gmail.com>
2020-01-29 14:13:45 +0100

    creation fontion de base CRUD

--- src/app/shared/services/representation.service.ts
+++ src/app/shared/services/representation.service.ts
@@ -1,4 +1,7 @@
+import { Representation } from './../models/representation';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,9 +9,35 @@ import { Injectable } from '@angular/core';
 export class RepresentationService {
 
   static URL = 'http://localhost:3000/representation/';
+  static URL_AUTH = '';
 
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getAll;
+
+
+  getAll(): Observable<Representation[]> {
+    return this.http.get<Representation[]>(RepresentationService.URL);
+  }
+
+  getRepresentationById(id): Observable<Representation> {
+    return this.http.get<Representation>(RepresentationService.URL + id);
+  }
+
+  getRepresentationForVisitor(): Observable<Representation[]> {
+    return this.http.get<Representation[]>(RepresentationService.URL + 'forMember');
+  }
+
+  createUser(newRepresentation) {
+    return this.http.post(RepresentationService.URL, newRepresentation);
+  }
+
+  updateRepresentation(representationToUpdate) {
+    const id = representationToUpdate.id;
+    return this.http.put(RepresentationService.URL + id, representationToUpdate);
+  }
+
+  deleteRepresentation(id) {
+    return this.http.delete(RepresentationService.URL + id);
+  }
 }
